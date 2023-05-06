dacf685478593e25545cead73a7079d1ae57fb02
huseyinalan <74354599+huseyinalan@users.noreply.github.com>
2023-05-06 03:26:53 +0300

    added utilities.css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/Screen/ScreenStore";
-import "./index.css";
-import App from "./App";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './redux/Screen/ScreenStore';
+import './index.css';
+import './assets/css/utilities.css';
+import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 let asdasd = persistStore(store);
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <PersistGate persistor={asdasd}>
-        <App />
-      </PersistGate>
-    </React.StrictMode>
-  </Provider>
+    <Provider store={store}>
+        <React.StrictMode>
+            <PersistGate persistor={asdasd}>
+                <App />
+            </PersistGate>
+        </React.StrictMode>
+    </Provider>
 );
