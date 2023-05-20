6773052f5149d49c9d9913731416b9d378239801
huseyinalan <74354599+huseyinalan@users.noreply.github.com>
2023-05-20 15:18:11 +0300

    improved auth

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,8 +1,20 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { useLocalStorage } from '../hook/useLocalStorage';
 
 const Context = createContext();
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(false);
+    const [user, setUser] = useLocalStorage("user", null);
+    
+    if(user)
+    {
+        let expirationTime = new Date(user.expirationTime);
+        let currentTime = new Date();
+
+        if(currentTime > expirationTime)
+        {
+            setUser(null);
+        }
+    }
 
     const data = {
         user,
